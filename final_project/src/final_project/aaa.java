5311da62f47b5aae4e33050a62eb67f75ef9bb4c
lizzieol <lizzie31@walla.com>
2016-12-03 11:57:08 +0200

    hh

--- final_project/src/final_project/aaa.java
+++ final_project/src/final_project/aaa.java
@@ -20,7 +20,7 @@ public class aaa {
 	public static void main(String [] args) throws IOException
 	{
 		
-	String str;
+	String str="aa";
 	String file_name;
 	String content = null;
 	 BufferedWriter writer = null;
